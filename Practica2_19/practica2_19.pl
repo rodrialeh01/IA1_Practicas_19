930e69da26c9ef78a2b10519ed5c51831e6d44fc
Rodri Hernández <2823897250101@ingenieria.usac.edu.gt>
2024-06-17 02:25:44 -0600

    Solucion Red Semantica

--- Practica2_19/practica2_19.pl
+++ Practica2_19/practica2_19.pl
@@ -1,32 +1,63 @@
-instancia_de(tweety, canario).
-instancia_de(pedro, halcon).
-instancia_de(juan, halcon).
-instancia_de(juancho, cocodrilo).
-instancia_de(pancho, pinguino).
+%-----------------HECHOS----------------------------
+% >>>>>> INSTANCIAS
+%Instancia de canciones a sus subgeneros
+%cancion -> barroca
+instancia_de(las_cuatro_estaciones, barroca).
+instancia_de(la_clave_bien_temperado, barroca).
+%cancion -> romanticismo
+instancia_de(concierto_para_piano2, romanticismo).
+%cancion -> dembow
+instancia_de(woman,dembow).
+%cancion ->techno
+instancia_de(strings_of_life, techno).
+%cancion -> EDM
+instancia_de(levels,edm).
+%cancion -> house
+instancia_de(one_more_time,house).
+%cancion -> rock pesado
+instancia_de(paranoid, rock_pesado).
+%cancion -> rock clasico
+instancia_de(i_want_to_break_free, rock_clasico).
+%cancion -> KPOP
+instancia_de(dynamite,kpop).
 
-%subclase(Subclase, Clase)
-subclase_de(ave, oviparo).
-subclase_de(reptil, oviparo).
-subclase_de(halcon, ave).
-subclase_de(canario, ave).
-subclase_de(ave, animal).
-subclase_de(reptil, animal).
-subclase_de(cocodrilo, reptil).
-subclase_de(pinguino, ave).
+% >>>>>> SUBCLASES
+%subgenero -> genero
+subclase_de(barroca,clasica).
+subclase_de(romanticismo,clasica).
+subclase_de(dembow,reggaeton).
+subclase_de(techno,electronica).
+subclase_de(edm,electronica).
+subclase_de(house,electronica).
+subclase_de(rock_pesado,electronica).
+subclase_de(rock_clasico,electronica).
+subclase_de(kpop, pop).
 
-%tiene_propiedad(clase1, propiedad, clase2)
-tiene_propiedad(canario, come, semillas).
-tiene_propiedad(ave, tiene, alas).
-tiene_propiedad(ave, tiene, plumas).
-tiene_propiedad(ave, puede, volar).
-tiene_propiedad(reptil, tiene, escamas).
+%genero -> musica
+subclase_de(clasica, musica).
+subclase_de(reggaeton, musica).
+subclase_de(electronica, musica).
+subclase_de(rock, musica).
+subclase_de(pop, musica).
 
-%%%%%reglas
+% >>>>>> PROPIEDADES
+%cancion - compuesta_por -> autor
+tiene_propiedad(las_cuatro_estaciones, compuesta_por, antonio_vivaldi).
+tiene_propiedad(la_clave_bien_temperado, compuesta_por, johann_sebastian_bach).
+tiene_propiedad(concierto_para_piano2, compuesta_por, rachmaninov).
+tiene_propiedad(woman, compuesta_por, doja_cat).
+tiene_propiedad(strings_of_life, compuesta_por, derrick_may).
+tiene_propiedad(levels, compuesta_por, avicii).
+tiene_propiedad(one_more_time, compuesta_por, daft_punk).
+tiene_propiedad(paranoid, compuesta_por, black_sabbath).
+tiene_propiedad(i_want_to_break_free, compuesta_por, queen).
+tiene_propiedad(dynamite, compuesta_por, bts).
+
+% >>>>>> REGLAS
 %regla para saber si un objeto es una instacia de una clase en concreto
 es(Clase, Obj):- instancia_de(Obj, Clase).
-
 es(Clase, Obj):- instancia_de(Obj, Clasep),
-                 subc(Clasep, Clase). 
+                 subc(Clasep, Clase).
 
 %Regla para saber si una clase es subclase de otra
 subc(C1, C2):- subclase_de(C1, C2).
