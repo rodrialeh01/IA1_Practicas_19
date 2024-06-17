2a16fff99d2d3f1c190ad4c4decae263862ea4c9
Rodri Hernández <2823897250101@ingenieria.usac.edu.gt>
2024-06-17 03:23:55 -0600

    Arreglando Archivo Prolog

--- Practica2_19/practica2_19.pl
+++ Practica2_19/practica2_19.pl
@@ -21,18 +21,19 @@ instancia_de(i_want_to_break_free, rock_clasico).
 %cancion -> KPOP
 instancia_de(dynamite,kpop).
 
-% >>>>>> SUBCLASES
+% >>>>>> SUB SUBCLASES
 %subgenero -> genero
-subclase_de(barroca,clasica).
-subclase_de(romanticismo,clasica).
-subclase_de(dembow,reggaeton).
-subclase_de(techno,electronica).
-subclase_de(edm,electronica).
-subclase_de(house,electronica).
-subclase_de(rock_pesado,electronica).
-subclase_de(rock_clasico,electronica).
-subclase_de(kpop, pop).
+sub_subclase_de(barroca,clasica).
+sub_subclase_de(romanticismo,clasica).
+sub_subclase_de(dembow,reggaeton).
+sub_subclase_de(techno,electronica).
+sub_subclase_de(edm,electronica).
+sub_subclase_de(house,electronica).
+sub_subclase_de(rock_pesado,electronica).
+sub_subclase_de(rock_clasico,electronica).
+sub_subclase_de(kpop, pop).
 
+% >>>>>> SUBCLASES
 %genero -> musica
 subclase_de(clasica, musica).
 subclase_de(reggaeton, musica).
@@ -41,28 +42,41 @@ subclase_de(rock, musica).
 subclase_de(pop, musica).
 
 % >>>>>> PROPIEDADES
-%cancion - compuesta_por -> autor
-tiene_propiedad(las_cuatro_estaciones, compuesta_por, antonio_vivaldi).
-tiene_propiedad(la_clave_bien_temperado, compuesta_por, johann_sebastian_bach).
-tiene_propiedad(concierto_para_piano2, compuesta_por, rachmaninov).
-tiene_propiedad(woman, compuesta_por, doja_cat).
-tiene_propiedad(strings_of_life, compuesta_por, derrick_may).
-tiene_propiedad(levels, compuesta_por, avicii).
-tiene_propiedad(one_more_time, compuesta_por, daft_punk).
-tiene_propiedad(paranoid, compuesta_por, black_sabbath).
-tiene_propiedad(i_want_to_break_free, compuesta_por, queen).
-tiene_propiedad(dynamite, compuesta_por, bts).
+%subgenero - compuesta_por -> autor
+tiene_propiedad(barroca, lo_compone, johann_sebastian_bach).
+tiene_propiedad(romanticismo, lo_compone, rachmaninov).
+tiene_propiedad(reggaeton, lo_canta, doja_cat).
+tiene_propiedad(techno, lo_reproduce, derrick_may).
+tiene_propiedad(edm, lo_reproduce, avicii).
+tiene_propiedad(house, lo_reproduce, daft_punk).
+tiene_propiedad(rock_pesado, lo_canta, black_sabbath).
+tiene_propiedad(rock_clasico, lo_canta, queen).
+tiene_propiedad(kpop, lo_canta, bts).
+tiene_propiedad(rock, surgio_en, estados_unidos).
+tiene_propiedad(pop, surgio_en, espania).
+tiene_propiedad(kpop, surgio_en, corea_del_sur).
 
 % >>>>>> REGLAS
 %regla para saber si un objeto es una instacia de una clase en concreto
 es(Clase, Obj):- instancia_de(Obj, Clase).
 es(Clase, Obj):- instancia_de(Obj, Clasep),
-                 subc(Clasep, Clase).
+                 sub_subc(Clasep, Clase).
+es(Clase, Obj):- instancia_de(Obj, Clasep),
+                 subc(Clasep, Clase2).
+es(Clase, Obj):- instancia_de(Obj, Clasep),
+                 sub_subc(Clasep, Clase2),
+                 subc(Clase2, Clase3),
+                 subc(Clase3, Clase).
 
 %Regla para saber si una clase es subclase de otra
 subc(C1, C2):- subclase_de(C1, C2).
 subc(C1, C2):- subclase_de(C1, C3),
                subc(C3, C2).
+
+%Regla para saber si una clase es subclase de otra
+sub_subc(C1, C2):- sub_subclase_de(C1, C2).
+sub_subc(C1, C2):- sub_subclase_de(C1, C3),
+                   sub_subc(C3, C2).
 
 %Regla para saber si un objeto tiene una propiedad
 propiedad(Obj, Prop):- es(Clase,Obj),
